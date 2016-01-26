494bc6990438961c8951d62dc6287d61842982d8
Ziya SARIKAYA <sarikayaziya@gmail.com>
2016-01-26 20:00:00 +0200

    code refactoring

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import Result from './result';
+import BaseExperiment from './base-experiment';
 import Experiment from './experiment';
 import Observation from './observation';
+import ResultPublisher from './result-publisher';
+import DateTimeProvider from './datetime-provider';
+import RandomProvider from './random-provider';
+import BadBehaviourError from './errors/bad-behaviour-error';
+import MissingBehaviourError from './errors/behaviour-missing-error';
 import BehaviourNotUniqueError from './errors/behaviour-not-unique-error';
 
 export {
+    ResultPublisher,
     Result,
+    BaseExperiment,
     Experiment,
+    BadBehaviourError,
     BehaviourNotUniqueError,
+    MissingBehaviourError,
+    RandomProvider,
+    DateTimeProvider,
     Observation
 };
